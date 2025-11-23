5b956b2a8e0367a6e959c89a1766b4be8b92e58f
longnhx1 <nguyeenhuynhlong@gmail.com>
2025-11-23 14:13:16 +0700

    Thêm Toolbox Guna UI

--- Cafe.GUI/Form1.Designer.cs
+++ Cafe.GUI/Form1.Designer.cs
@@ -29,12 +29,23 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.guna2AnimateWindow1 = new Guna.UI2.WinForms.Guna2AnimateWindow(this.components);
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2AnimateWindow guna2AnimateWindow1;
     }
 }
 
--- Cafe.GUI/Form1.cs
+++ Cafe.GUI/Form1.cs
@@ -16,5 +16,6 @@ namespace Cafe.GUI
         {
             InitializeComponent();
         }
+
     }
 }
